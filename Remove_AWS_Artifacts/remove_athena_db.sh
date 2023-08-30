93d99faea713c761e7fa4f56b8f741c19215ffe0
lingnath <32849838+lingnath@users.noreply.github.com>
2023-08-30 11:49:37 -0700

    Update remove_athena_db.sh

--- Remove_AWS_Artifacts/remove_athena_db.sh
+++ Remove_AWS_Artifacts/remove_athena_db.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-athena_db=$(cat ../config_file.toml | grep 'athena_db' | awk -F"=" '{print $2}' | tr -d "'")
-region=$(cat ../config_file.toml | grep 'region' | awk -F"=" '{print $2}' | tr -d "'")
-s3_bucket_output=$(cat ../config_file.toml | grep 's3_bucket_output' | awk -F"=" '{print $2}' | tr -d "'")
+athena_db=$(cat ../config_file.toml | grep 'athena_db' | awk -F "=" '{print $2}' | tr -d "'" | tr -d " ")
+region=$(cat ../config_file.toml | grep 'region' | awk -F "=" '{print $2}' | tr -d "'" | tr -d " ")
+s3_bucket_output=$(cat ../config_file.toml | grep 's3_bucket_output' | awk -F "=" '{print $2}' | tr -d "'" | tr -d " ")
 
 # Remove athena database for this project
 aws glue delete-database --name ${athena_db} \
