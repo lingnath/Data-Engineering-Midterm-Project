a1978afe23768aa6c8a293547aad7df61bd540e5
lingnath <32849838+lingnath@users.noreply.github.com>
2023-08-30 11:34:42 -0700

    Update airflow_ui_setup.sh

--- Airflow_EMR/airflow_ui_setup.sh
+++ Airflow_EMR/airflow_ui_setup.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-access_key=$(cat .env | grep 'ACCESS_KEY' | awk -F " = " '{print $2}' | tr -d '"')
-secret_access_key=$(cat .env | grep 'SECRET_KEY' | awk -F " = " '{print $2}' | tr -d '"')
-region=$(cat config_file.toml | grep 'region' | awk -F"=" '{print $2}' | tr -d "'")
-dag_name=$(cat config_file.toml | grep 'dag_name' | awk -F"=" '{print $2}' | tr -d "'")
+access_key=$(cat .env | grep 'ACCESS_KEY' | awk -F "=" '{print $2}' | tr -d "'" | tr -d " ")
+secret_access_key=$(cat .env | grep 'SECRET_KEY' | awk -F "=" '{print $2}' | tr -d "'" | tr -d " ")
+region=$(cat config_file.toml | grep 'region' | awk -F "=" '{print $2}' | tr -d "'" | tr -d " ")
+dag_name=$(cat config_file.toml | grep 'dag_name' | awk -F "=" '{print $2}' | tr -d "'" | tr -d " ")
 extras='{"region_name":''"'"${region}"'"''}'
 
 # Create the aws connection
