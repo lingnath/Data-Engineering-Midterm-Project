c3ab05c48c55d98fc6c8ee1ba011ae82d37f4ef8
lingnath <32849838+lingnath@users.noreply.github.com>
2023-08-30 11:50:20 -0700

    Update remove_glue_crawler.sh

--- Remove_AWS_Artifacts/remove_glue_crawler.sh
+++ Remove_AWS_Artifacts/remove_glue_crawler.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-aws_region=$(cat ../config_file.toml | grep 'region' | awk -F"=" '{print $2}' | tr -d "'")
-glue_crawler_name=$(cat ../config_file.toml | grep 'glue_crawler_name' | awk -F"=" '{print $2}' | tr -d "'")
+aws_region=$(cat ../config_file.toml | grep 'region' | awk -F "=" '{print $2}' | tr -d "'" | tr -d " ")
+glue_crawler_name=$(cat ../config_file.toml | grep 'glue_crawler_name' | awk -F "=" '{print $2}' | tr -d "'" | tr -d " ")
 
 # Deletes the glue crawlers for each of the tables (calendar, fact, product, and store tables)
 for table in calendar fact product store;
