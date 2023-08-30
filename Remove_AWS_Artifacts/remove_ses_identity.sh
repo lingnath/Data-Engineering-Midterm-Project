210820ff3a284872ddbca1e50e73adf03b6a2ca8
lingnath <32849838+lingnath@users.noreply.github.com>
2023-08-30 11:52:47 -0700

    Update remove_ses_identity.sh

--- Remove_AWS_Artifacts/remove_ses_identity.sh
+++ Remove_AWS_Artifacts/remove_ses_identity.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-email_address=$(cat ../config_file.toml | grep 'sender' | awk -F"=" '{print $2}' | tr -d "'")
-aws_region=$(cat ../config_file.toml | grep 'region' | awk -F"=" '{print $2}' | tr -d "'")
+email_address=$(cat ../config_file.toml | grep 'sender' | awk -F "=" '{print $2}' | tr -d "'" | tr -d " ")
+aws_region=$(cat ../config_file.toml | grep 'region' | awk -F "=" '{print $2}' | tr -d "'" | tr -d " ")
 
 # Removes your email from SES
 aws sesv2 delete-email-identity \
