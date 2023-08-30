646ae26884db85fd931bc8652829572f61c0693c
lingnath <32849838+lingnath@users.noreply.github.com>
2023-08-30 11:39:04 -0700

    Update create_athena_db.sh

--- Create_AWS_Artifacts/create_athena_db.sh
+++ Create_AWS_Artifacts/create_athena_db.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-athena_db=$(cat ../config_file.toml | grep 'athena_db' | awk -F"=" '{print $2}' | tr -d "'")
-region=$(cat ../config_file.toml | grep 'region' | awk -F"=" '{print $2}' | tr -d "'")
-s3_bucket_output=$(cat ../config_file.toml | grep 's3_bucket_output' | awk -F"=" '{print $2}' | tr -d "'")
+athena_db=$(cat ../config_file.toml | grep 'athena_db' | awk -F"=" '{print $2}' | tr -d "'" | tr -d " ")
+region=$(cat ../config_file.toml | grep 'region' | awk -F"=" '{print $2}' | tr -d "'" | tr -d " ")
+s3_bucket_output=$(cat ../config_file.toml | grep 's3_bucket_output' | awk -F"=" '{print $2}' | tr -d "'" | tr -d " ")
 
 # Creates the database in Athena for this project
 aws athena start-query-execution --query-string "CREATE database ${athena_db}" \
