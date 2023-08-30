e03f20bdb78e1b5b070abf67ef48556922469896
lingnath <32849838+lingnath@users.noreply.github.com>
2023-08-30 11:42:21 -0700

    Update create_ses_identity.sh

--- Create_AWS_Artifacts/create_ses_identity.sh
+++ Create_AWS_Artifacts/create_ses_identity.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-email_address=$(cat ../config_file.toml | grep 'sender' | awk -F"=" '{print $2}' | tr -d "'")
-aws_region=$(cat ../config_file.toml | grep 'region' | awk -F"=" '{print $2}' | tr -d "'")
+email_address=$(cat ../config_file.toml | grep 'sender' | awk -F"=" '{print $2}' | tr -d "'" | tr -d " ")
+aws_region=$(cat ../config_file.toml | grep 'region' | awk -F"=" '{print $2}' | tr -d "'" | tr -d " ")
 
 # Configure email in SES
 aws sesv2 create-email-identity \
