2bd17d97d3aa98a482484d481ef33ff56816f931
Nathan Ling <32849838+lingnath@users.noreply.github.com>
2025-06-29 19:28:30 -0700

    Update install_docker.sh

--- Software_Installations/install_docker.sh
+++ Software_Installations/install_docker.sh
@@ -2,7 +2,7 @@
 
 # Please replace apt-get with yum if you are using Amazon AMI, which uses RHEL
 sudo apt-get update
-sudo apt-get remove docker docker-engine docker.io
+# sudo apt-get remove docker docker-engine docker.io
 sudo apt-get install docker.io -y
 sudo systemctl start docker
 sudo systemctl enable docker
