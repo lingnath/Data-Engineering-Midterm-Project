536a627e7bc9423f96303736e215ed87026ce8c3
Nathan Ling <32849838+lingnath@users.noreply.github.com>
2025-07-04 09:23:34 -0700

    Update create_lambda_schedule_trigger.sh

--- Create_AWS_Artifacts/create_lambda_schedule_trigger.sh
+++ Create_AWS_Artifacts/create_lambda_schedule_trigger.sh
@@ -6,7 +6,7 @@ account_id=$(cat ../config_file.toml | grep 'account_id' | awk -F "=" '{print $2
 lambda_function_name=$(cat ../config_file.toml | grep 'lambda_function_name' | awk -F "=" '{print $2}' | tr -d "'" | tr -d " ")
 
 # Define Lambda Payload
-# payload='{"action":"start"}'
+payload='{\"action\":\"start\"}'
 
 # Create event rule trigger in Amazon Eventbridge so that Lambda could be invoked at a specific time interval
 aws events put-rule \
@@ -31,8 +31,8 @@ lambda_function_arn="arn:aws:lambda:${aws_region}:${account_id}:function:${lambd
 echo "[" >> targets.json
 echo -e "\t{" >> targets.json
 echo '      "Id": "1",' >> targets.json
-echo '      "Arn":"'${lambda_function_arn}'"' >> targets.json
-# echo '    "Input": "'"${payload}"'"' >> targets.json
+echo '      "Arn":"'${lambda_function_arn}'",' >> targets.json
+echo '    "Input": "'"${payload}"'"' >> targets.json
 echo -e "\t}" >> targets.json
 echo "]" >> targets.json
 
